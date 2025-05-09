bccb0e7371ce0d1dab83c38f4ed286166609c8b1
chilala <chekkitapp@gmail.com>
2025-05-09 09:39:52 -0400

    create endpoint for getcourses and getcourse by id

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController : ControllerBase
+    {
+        // You can add common methods or properties here (e.g., logger, user info, etc.)
+        
+    }
+}
---
+++ API/Controllers/CourseController.cs
@@ -0,0 +1,36 @@
+﻿using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class CoursesController : BaseController
+    {
+        private readonly StoreContext _context;
+
+        public CoursesController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/courses
+        [HttpGet]
+        public async Task<IActionResult> GetCourses()
+        {
+            var courses = await _context.Courses.ToListAsync();
+            return Ok(courses);
+        }
+
+        // GET: api/courses/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCourse(Guid id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+
+            if (course == null)
+                return NotFound();
+
+            return Ok(course);
+        }
+    }
+}
